603eae1b56d5c7cca78608118a19e8987f4da9ee
Alice Feng <feng.a.g@gmail.com>
2017-02-18 13:51:19 -0500

    Add more films.

--- data/getSongTimes.py
+++ data/getSongTimes.py
@@ -42,16 +42,17 @@ def minimize_subdata(film):
 
     if film in ['Pinocchio', 'Bambi', 'The Aristocats']:
         subdata = data.loc[data.text.str.contains("♪", na=False)]
-    elif film in ['Cinderella']:
+    elif film in ['Cinderella', 'One Hundred and One Dalmatians']:
         subdata = data.loc[data.text.str.contains("<i>", na=False)]
-    elif film in ['Alice in Wonderland']:
+    elif film in ['Alice in Wonderland', 'Sleeping Beauty']:
         subdata = data.loc[data.text.str.contains("♫", na=False)]
     elif film in ['The Sword in the Stone']:
         subdata = data.loc[data.text.str.contains("%%", na=False)]
+    elif film in ['Lady and the Tramp']:
+        subdata = data.loc[data.text.str.contains("#", na=False)]
     else:
         subdata = data
     
-    # reset index so we can iterate through the dataframe using a counter
     return subdata
                 
 
@@ -106,7 +107,7 @@ def line_match(sub_line, lyric_line):
 disney_films = pd.read_csv('disney_animated_feature_films.csv')
 song_lyrics = pd.read_csv('song_lyrics.csv')
     
-films = ['Snow White and the Seven Dwarfs', 'Pinocchio']
+films = disney_films.Title[disney_films['Include?']=='Y']
 match = []
     
 for film in films:
@@ -174,24 +175,5 @@ for film in films:
     # remove "songs" with length of less than 10 sec
     song_times = song_times[song_times.Length > datetime.timedelta(seconds=10)]
     
-    
-### Prep dataset for plotting
-    
-# add on runtimes to song_times
-film_lengths = disney_films[['Film Number', 'Title', 'Runtime']]
-
-final_output = song_times.merge(film_lengths, left_on='Film', right_on='Title')
-
-# convert runtimes to seconds
-final_output['Runtime_Seconds'] = pd.to_numeric(final_output['Runtime'].str.split().str[0])*60
-
-# calculate percent of the film that has elapsed before each song begins and ends
-final_output['Start_pos'] = (pd.to_datetime(final_output.Start_time, format='%H:%M:%S,%f').dt.hour*3600 + 
-                             pd.to_datetime(final_output.Start_time, format='%H:%M:%S,%f').dt.minute*60 + 
-                             pd.to_datetime(final_output.Start_time, format='%H:%M:%S,%f').dt.second)/final_output['Runtime_Seconds']
-final_output['End_pos'] = (pd.to_datetime(final_output.End_time, format='%H:%M:%S,%f').dt.hour*3600 + 
-                           pd.to_datetime(final_output.End_time, format='%H:%M:%S,%f').dt.minute*60 + 
-                           pd.to_datetime(final_output.End_time, format='%H:%M:%S,%f').dt.second)/final_output['Runtime_Seconds']
-
 # write out final output
-final_output.to_csv('final_matches.csv', index=False)
+song_times.to_csv('final_matches.csv', index=False)
